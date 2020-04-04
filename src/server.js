3a878fa791afda41373cf3e16146ca77dc6b2877
evandrotvc <evandro-tvc@hotmail.com>
2020-04-04 16:16:52 -0300

    test

--- src/server.js
+++ src/server.js
@@ -1,11 +1,4 @@
 const app = require('./app')
 const {User} = require('./app/models/User')
 
-/*User.create({
-     name:"evandro" , 
-     email: "evandrotvc123@gmail.com" ,
-     password_hash: "123"
-     })*/
-
-
 app.listen(process.env.PORT || 3000)
